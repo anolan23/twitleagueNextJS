c6b141df25ca3c8b3340adaff94721786593e8d1
Aaron <aaron@cpe-172-100-16-35.twcny.res.rr.com>
2021-09-09 14:30:38 -0500

    updated db options

--- db/pool.js
+++ db/pool.js
@@ -1,24 +1,24 @@
-import pg from "pg"
+import pg from "pg";
 
 const options = () => {
-    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-        return {
-            user: 'aaron',
-            host: 'localhost',
-            database: 'twitleague',
-            password: '',
-            port: 5432,
-        };
-    } else {
-        return {
-            user: 'twitleague',
-            host: 'twitleague.chynzy0qazsi.us-east-2.rds.amazonaws.com',
-            database: 'twitleague',
-            password: 'Beta095458!',
-            port: 5432,
-        }
-    }
-}
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    return {
+      user: "aaron",
+      host: "localhost",
+      database: "twitleague",
+      password: "",
+      port: 5432,
+    };
+  } else {
+    return {
+      user: "twitleague",
+      host: "twitleague.chynzy0qazsi.us-east-2.rds.amazonaws.com",
+      database: "twitleague",
+      password: "Zeta095458!",
+      port: 5432,
+    };
+  }
+};
 
 const pool = new pg.Pool(options());
 
