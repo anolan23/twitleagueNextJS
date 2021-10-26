18a7d8592bf466dd38ff75637706c132a2478651
anolan23 <anolan23@yahoo.com>
2021-10-26 13:42:01 -0500

    assfsa

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,22 @@
 import pg from "pg";
+import axios from "axios";
 
-const options = () => {
+async function getDatabaseURL() {
+  const results = await axios.get(
+    "https://api.heroku.com/apps/twitleague-db/config-vars",
+    {
+      headers: {
+        Accept: "application/vnd.heroku+json; version=3",
+        Authorization: `Bearer ${process.env.HEROKU_API_KEY}`,
+      },
+    }
+  );
+  return results.data.DATABASE_URL;
+}
+
+getDatabaseURL();
+
+const options = async () => {
   if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
     return {
       user: "aaron",
@@ -11,8 +27,7 @@ const options = () => {
     };
   } else {
     return {
-      connectionString:
-        "postgres://zaqshrnkoddgui:77cf7e12d6e458c3b42e40e5db54844be4e045852cf203f94057b5ccfd748ecf@ec2-54-172-169-87.compute-1.amazonaws.com:5432/d4q9qkfjsq5fvm",
+      connectionString: await getDatabaseURL(),
       ssl: {
         rejectUnauthorized: false,
       },
