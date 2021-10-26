9ce7bc73593b8f792fb965750cb71b3df7212fac
anolan23 <anolan23@yahoo.com>
2021-10-26 13:50:25 -0500

    ok

--- db/pool.js
+++ db/pool.js
@@ -1,22 +1,19 @@
 import pg from "pg";
-import axios from "axios";
 
-async function getDatabaseURL() {
-  const results = await axios.get(
-    "https://api.heroku.com/apps/twitleague-db/config-vars",
-    {
-      headers: {
-        Accept: "application/vnd.heroku+json; version=3",
-        Authorization: `Bearer ${process.env.HEROKU_API_KEY}`,
-      },
-    }
-  );
-  return results.data.DATABASE_URL;
-}
+// async function getDatabaseURL() {
+//   const results = await axios.get(
+//     "https://api.heroku.com/apps/twitleague-db/config-vars",
+//     {
+//       headers: {
+//         Accept: "application/vnd.heroku+json; version=3",
+//         Authorization: `Bearer ${process.env.HEROKU_API_KEY}`,
+//       },
+//     }
+//   );
+//   return results.data.DATABASE_URL;
+// }
 
-getDatabaseURL();
-
-const options = async () => {
+const options = () => {
   if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
     return {
       user: "aaron",
@@ -27,7 +24,8 @@ const options = async () => {
     };
   } else {
     return {
-      connectionString: await getDatabaseURL(),
+      connectionString:
+        "postgres://zaqshrnkoddgui:77cf7e12d6e458c3b42e40e5db54844be4e045852cf203f94057b5ccfd748ecf@ec2-54-172-169-87.compute-1.amazonaws.com:5432/d4q9qkfjsq5fvm",
       ssl: {
         rejectUnauthorized: false,
       },
