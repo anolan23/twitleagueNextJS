5d6479d0c3a53d926ca58472f9b798ef421ad0f7
anolan23 <anolan23@yahoo.com>
2021-10-26 11:23:38 -0500

    rgsfsad

--- db/pool.js
+++ db/pool.js
@@ -10,11 +10,13 @@ const options = () => {
       port: 5432,
     };
   } else {
-    return "postgres://zaqshrnkoddgui:77cf7e12d6e458c3b42e40e5db54844be4e045852cf203f94057b5ccfd748ecf@ec2-54-172-169-87.compute-1.amazonaws.com:5432/d4q9qkfjsq5fvm?sslmode=require";
+    return {
+      connectionString:
+        "postgres://zaqshrnkoddgui:77cf7e12d6e458c3b42e40e5db54844be4e045852cf203f94057b5ccfd748ecf@ec2-54-172-169-87.compute-1.amazonaws.com:5432/d4q9qkfjsq5fvm?sslmode=require",
+    };
   }
 };
 
 const pool = new pg.Pool(options());
-console.log(pool);
 
 export default pool;
